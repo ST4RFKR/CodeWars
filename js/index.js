e3bbded1e0cebd75808cc6c59ee6c793ce0c1ef4
Евгений Супруненко <admin@MacBook-Pro-Evgenij.local>
2024-08-15 11:53:34 +0300

    If you cant sleep, just count sheep!!

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ var number = function (array) {
 };
 const number1 = (array) => array.map((item, idx) => `${idx + 1}:${item}`);
 
+//Sum of positive
 // You get an array of numbers, return the sum of all of the positives ones.
 // Example [1,-4,7,12] => 1 + 7 + 12 = 20
 // Note: if there is nothing to sum, the sum is default to 0.
@@ -20,3 +21,21 @@ const positiveSum = (arr) => arr.filter((i) => i > 0).reduce((acc, item) => (acc
 
 positiveSum([1, -4, 7, 12]);
 console.log(positiveSum([1, -4, 7, 12]));
+
+// 8 kyu If you can't sleep, just count sheep!!
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+var countSheep = function (num) {
+  let str = '';
+  if (num < 0) return;
+  if (num === 0) return str;
+  for (let i = 0; i < num; i++) {
+    console.log(i);
+    str = str + `${i + 1} sheep...`;
+  }
+  return str;
+};
+console.log(countSheep(4));
+console.log(countSheep(0));
+console.log(countSheep(1));
