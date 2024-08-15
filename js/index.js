b383d109d462551acab106c9fb2164c5da6c82ff
Евгений Супруненко <admin@MacBook-Pro-Evgenij.local>
2024-08-15 11:34:57 +0300

    Sum of positive

--- js/index.js
+++ js/index.js
@@ -10,4 +10,13 @@ var number = function (array) {
 
   return fixedArr;
 };
-const number = (array) => array.map((item, idx) => `${idx + 1}:${item}`);
+const number1 = (array) => array.map((item, idx) => `${idx + 1}:${item}`);
+
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+const positiveSum = (arr) => arr.filter((i) => i > 0).reduce((acc, item) => (acc += item), 0);
+
+positiveSum([1, -4, 7, 12]);
+console.log(positiveSum([1, -4, 7, 12]));
