c489ddaf9208789adf6455c6437598b848270c04
Eugene <admin@MacBook-Pro-Evgenij.local>
2024-09-02 07:12:59 +0300

    // 8 kyu // Removing Elements

--- js/index.js
+++ js/index.js
@@ -233,3 +233,17 @@ function accum(s) {
     .join('-');
   return result;
 }
+// 8 kyu
+// Removing Elements
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+function removeEveryOther(arr) {
+  let filteredArr = arr.filter((item, idx) => {
+    return idx % 2 === 0;
+  });
+  return filteredArr;
+}
