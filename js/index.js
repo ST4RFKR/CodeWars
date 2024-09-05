1f5635590c4fe76b135a6361f465fb998e674772
Eugene <admin@MacBook-Pro-Evgenij.local>
2024-09-05 10:22:45 +0300

    8 kyu Beginner - Lost Without a Map

--- js/index.js
+++ js/index.js
@@ -281,3 +281,11 @@ function addBinary(a, b) {
 }
 
 console.log(addBinary(5, 9));
+// 8 kyu
+// Beginner - Lost Without a Map
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+function maps(x) {
+  return x.map((item) => item * 2);
+}
