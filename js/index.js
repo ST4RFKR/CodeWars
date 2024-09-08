31855ae9eaf86442704e41ee4d3eb259d2e14b48
Eugene <admin@MacBook-Pro-Evgenij.local>
2024-09-08 09:57:12 +0300

    6 kyu Count the smiley faces!

--- js/index.js
+++ js/index.js
@@ -343,3 +343,29 @@ function likes(names) {
 function makeUpperCase(str) {
   return str.toUpperCase();
 }
+// 6 kyu
+// Count the smiley faces!
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+// Rules for a smiling face:
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+let validSmile = [':)', ';)', ':-)', ';-)', ':~)', ';~)', ':D', ';D', ':-D', ';-D', ':~D', ';~D'];
+function countSmileys(arr) {
+  let count = 0;
+  arr.forEach((item) => {
+    for (let i = 0; i < validSmile.length; i++) {
+      if (item === validSmile[i]) {
+        count++;
+      }
+    }
+  });
+  return count;
+}
+// console.log(countSmileys([':D', ':~)', ';~D', ':)']));
+// console.log(countSmileys([':)', ':(', ':D', ':O', ':;']));
+// console.log(countSmileys([';]', ':[', ';*', ':$', ';-D']));
