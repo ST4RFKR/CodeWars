6627b0ef13bb147497e0f5495af81fa98d2ed0b6
Eugene <admin@MacBook-Pro-Evgenij.local>
2024-09-14 07:06:08 +0300

    twiceAsOld

--- js/index.js
+++ js/index.js
@@ -397,3 +397,30 @@ function fakeBin(x) {
     .map((item) => (item <= '4' ? '0' : '1'))
     .join('');
 }
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 0;
+  }
+}
+
+function removeChar(str) {
+  return str.slice(1, -1);
+}
+
+function smash(words) {
+  return words.join(' ');
+}
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  let difference = 2 * sonYearsOld - dadYearsOld;
+  return Math.abs(difference);
+}
